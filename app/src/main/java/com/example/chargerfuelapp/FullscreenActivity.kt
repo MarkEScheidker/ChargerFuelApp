751dd29244f084a2f72f1768dfebd054af100a37
MarkEScheidker <mark.scheidker@gmail.com>
2023-02-21 17:52:45 -0600

    fixed display issue where exiting the app would mess up the position of the window

--- app/src/main/java/com/example/chargerfuelapp/FullscreenActivity.kt
+++ app/src/main/java/com/example/chargerfuelapp/FullscreenActivity.kt
@@ -55,9 +55,12 @@ class FullscreenActivity : AppCompatActivity() {
                 }
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
                     webView.setSystemUiVisibility(
-                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
                                 View.SYSTEM_UI_FLAG_FULLSCREEN or
-                                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                                View.SYSTEM_UI_FLAG_IMMERSIVE
                     )
                 }
             }
