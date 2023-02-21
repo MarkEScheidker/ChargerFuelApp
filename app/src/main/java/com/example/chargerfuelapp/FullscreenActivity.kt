504a8e86765e8687bd4805061401d9d05e8a9542
MarkEScheidker <mark.scheidker@gmail.com>
2023-02-20 21:38:10 -0600

    readability changes, removed unused code

--- app/src/main/java/com/example/chargerfuelapp/FullscreenActivity.kt
+++ app/src/main/java/com/example/chargerfuelapp/FullscreenActivity.kt
@@ -6,9 +6,7 @@ import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.view.MotionEvent
 import android.view.View
-import android.view.WindowInsets
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.LinearLayout
@@ -22,32 +20,6 @@ class FullscreenActivity : AppCompatActivity() {
     private lateinit var fullscreenContentControls: LinearLayout
     private val hideHandler = Handler(Looper.myLooper()!!)
     private lateinit var webView: WebView
-
-    @SuppressLint("InlinedApi")
-    private val hidePart2Runnable = Runnable {
-        // Delayed removal of status and navigation bar
-        if (Build.VERSION.SDK_INT >= 30) {
-            fullscreenContent.windowInsetsController?.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
-        } else {
-            // Note that some of these constants are new as of API 16 (Jelly Bean)
-            // and API 19 (KitKat). It is safe to use them, as they are inlined
-            // at compile-time and do nothing on earlier devices.
-            fullscreenContent.systemUiVisibility =
-                View.SYSTEM_UI_FLAG_LOW_PROFILE or
-                        View.SYSTEM_UI_FLAG_FULLSCREEN or
-                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-        }
-    }
-    private val showPart2Runnable = Runnable {
-        // Delayed display of UI elements
-        supportActionBar?.show()
-        fullscreenContentControls.visibility = View.VISIBLE
-    }
-    private var isFullscreen: Boolean = false
-
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -63,37 +35,37 @@ class FullscreenActivity : AppCompatActivity() {
                         View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 )
 
-            // Set the layout for the activity
-            setContentView(R.layout.activity_fullscreen)
+        // Set the layout for the activity
+        setContentView(R.layout.activity_fullscreen)
 
-            // Find the WebView in the layout
-            webView = findViewById<WebView>(R.id.webview)
+        // Find the WebView in the layout
+        webView = findViewById<WebView>(R.id.webview)
 
-            // Enable JavaScript in the WebView
-            webView.settings.javaScriptEnabled = true
+        // Enable JavaScript in the WebView
+        webView.settings.javaScriptEnabled = true
 
-            // Load your website in the WebView
-            webView.clearCache(true)
-            webView.clearHistory()
-            webView.loadUrl("https://charger.food.is")
+        // Load website in the WebView and delete the cache and history
+        webView.clearCache(true)
+        webView.clearHistory()
+        webView.loadUrl("https://charger.food.is")
 
-            // Set a WebViewClient to handle page navigation
-            webView.webViewClient = object : WebViewClient() {
-                override fun onPageFinished(view: WebView?, url: String?) {
-                    super.onPageFinished(view, url)
+        // Set a WebViewClient to handle page navigation
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
 
-                    // Once the page is loaded, hide the URL bar
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                        webView.setSystemUiVisibility(
-                            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                                    View.SYSTEM_UI_FLAG_FULLSCREEN or
-                                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        )
-                    }
+                // Once the page is loaded, hide the URL bar
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                    webView.setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                                View.SYSTEM_UI_FLAG_FULLSCREEN or
+                                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    )
                 }
             }
-
         }
+    }
+
     override fun onBackPressed() {
         if (webView.canGoBack()) {
             webView.goBack()
@@ -105,6 +77,4 @@ class FullscreenActivity : AppCompatActivity() {
     override fun onPostCreate(savedInstanceState: Bundle?) {
         super.onPostCreate(savedInstanceState)
     }
-
-
 }
