d1417dfd6f44dcf605ac62ce71c931a449e4a8f9
MarkEScheidker <mark.scheidker@gmail.com>
2023-02-21 22:01:44 -0600

    added animation to fade out the splash screen

--- app/src/main/java/com/example/chargerfuelapp/FullscreenActivity.kt
+++ app/src/main/java/com/example/chargerfuelapp/FullscreenActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.View
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
@@ -49,7 +51,7 @@ class FullscreenActivity : AppCompatActivity() {
                 super.onPageFinished(view, url)
 
                 // Once the page is loaded, hide the splash screen and show the WebView
-                hideHandler.postDelayed(hideRunnable,0)
+                hideHandler.postDelayed(hideRunnable, 500)
                 if (!splashShown) {
                     hideSplash()
                 }
@@ -76,15 +78,45 @@ class FullscreenActivity : AppCompatActivity() {
     }
 
     private fun showSplash() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            splashView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                        View.SYSTEM_UI_FLAG_FULLSCREEN or
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                        View.SYSTEM_UI_FLAG_IMMERSIVE
+            )
+        }
         splashView.visibility = View.VISIBLE
+        val anim = AlphaAnimation(0.0f, 1.0f)
+        anim.duration = 500
+        splashView.startAnimation(anim)
         splashShown = true
-    }
 
+    }
     private fun hideSplash() {
-        splashView.visibility = View.GONE
         webView.visibility = View.VISIBLE
-        splashShown = false
-        hideHandler.removeCallbacks(hideRunnable)
+
+        // Add a delay of 100ms before starting the fade animation
+        Handler().postDelayed({
+            val anim = AlphaAnimation(1.0f, 0.0f)
+            anim.duration = 500
+            splashView.startAnimation(anim)
+            anim.setAnimationListener(object : Animation.AnimationListener {
+                override fun onAnimationStart(animation: Animation?) {}
+                override fun onAnimationRepeat(animation: Animation?) {}
+                override fun onAnimationEnd(animation: Animation?) {
+                    splashView.visibility = View.GONE
+                    splashShown = false
+                }
+            })
+        }, 100)
+
+        Handler().postDelayed({
+            binding.root.removeView(splashView)
+            splashShown = false
+        }, 650)
     }
 
     override fun onPostCreate(savedInstanceState: Bundle?) {
